36190a3d42173bec32926f04b35eacff45fd9da9
MichaelWK04 <michaelwalker1358@gmail.com>
2025-07-16 13:49:56 -0400

    Created new merge_reports algorithm

--- NZ_Script_1.py
+++ NZ_Script_1.py
@@ -1,39 +1,55 @@
 import pandas as pd
 from tkinter import messagebox, filedialog
 import json
+import os
 
 CEFID = "5153"
 UWOID = "5163"
 NZID = "5151"
 INPUT_FOLDER = "./Inputs/"
-LIBRARIES_JSON = "libraries.json"
-LIBRARY_FILE_NAME = "testfile.txt"
+OUTPUT_FOLDER = "./Outputs/"
+INSTITUTIONS_JSON = "institutions.json"
+INPUT_CSV_FILE_NAME = "testfile.txt"
+OUTPUT_CSV_FILE_NAME = "bibprocess.csv"
 
 #Merge two Bib Processing Reports and dedupe using NZ ID, then split into NZ and IZ reports
 def merge_reports():
     #Prompt user for .txt files and open both
-    # messagebox.showinfo(title=None, message='Please select the Bib Processing Reports to load and compare.')
-    # fn1 = filedialog.askopenfilename()
-    # f1 = open(fn1, 'r')
-    # fn2 = filedialog.askopenfilename()
-    # f2 = open(fn2, 'r')
 
-    with open(LIBRARIES_JSON) as libraries_json:
-        libraries = json.load(libraries_json)
+    with open(INSTITUTIONS_JSON) as institutions_json:
+        institutions = json.load(institutions_json)
 
-    merged = pd.DataFrame(columns=["JobID", "Network Id", "Existing 035a", "Incoming 035a", "Action"])
+    columns=["JobID", "Network Id", "Existing 035a", "Incoming 035a", "Action"]
+    merged = pd.DataFrame(columns=columns)
 
-    for library in libraries:
-        id = library["id"]
-        code = library["code"]
-
-        with open(f"{INPUT_FOLDER}{code}{LIBRARY_FILE_NAME}") as csv:
-            df = pd.read_csv(csv, sep="|", header=None, dtype=str, names=["JobID", "Network Id", "Existing 035a", "Incoming 035a", "Action"])
+    for file in os.listdir(INPUT_FOLDER):
+        with open(os.path.join(INPUT_FOLDER, file)) as csv:
+            df = pd.read_csv(csv, sep="|", header=None, dtype=str, names=columns)
 
         merged = pd.merge(merged, df, how="outer")
         merged.drop_duplicates(subset=["Network Id"], keep="first", inplace = True)
         merged["Network Id"] = merged["Network Id"].astype(str)
-    return
+
+    for library in institutions:
+        id = library["id"]
+        code = library["code"]
+
+        bibprocess = merged[merged['Network Id'].str.endswith(id)]
+        bibprocess.to_csv(f"{OUTPUT_FOLDER}test{code}{OUTPUT_CSV_FILE_NAME}", mode = "w", index = False)
+        print(f"{code} records saved to {code}bibprocess.csv")
+
+    nz = merged[merged['Network Id'].str.endswith(NZID)]
+    nz.to_csv(f"{OUTPUT_FOLDER}testbibprocessmerged.csv", mode = "w", index = False)
+    print("NZ records saved to testbibprocessmerged.csv")
+
+
+def merge_reports_old():
+    messagebox.showinfo(title=None, message='Please select the Bib Processing Reports to load and compare.')
+    fn1 = filedialog.askopenfilename()
+    f1 = open(fn1, 'r')
+    fn2 = filedialog.askopenfilename()
+    f2 = open(fn2, 'r')
+
     #Create dataframes with labelled columns (copied from UWO code)
     df1 = pd.read_csv(f1, sep="|", header=None, dtype=str)
     df1.columns = ["JobID", "Network Id", "Existing 035a", "Incoming 035a", "Action"]
@@ -65,9 +81,10 @@ def merge_reports():
     NZ.to_csv("bibprocessmerged.csv", mode = "w", index = False)
     print("NZ records saved to bibprocessmerged.csv")
 
+
 def compare_OCLC(): #Copied from UWO code
     #Read the BIB processing report. Add the filepath to the txt file
-    data = pd.read_csv('bibprocessmerged.csv')
+    data = pd.read_csv(f'{OUTPUT_FOLDER}bibprocessmerged.csv')
 
     #make a dataframe from the BIB processing report and set the format as text for columns b and c
     df = pd.DataFrame(data, columns= ['JobID', 'Network Id', 'Existing 035a', 'Incoming 035a', 'Action'])
@@ -81,7 +98,7 @@ def compare_OCLC(): #Copied from UWO code
     print(SAME)
 
     #Create the comparison_fileIZ file. Add the file path to the Excel file
-    writer = pd.ExcelWriter('comparison_file_IZ.xlsx', engine='xlsxwriter')
+    writer = pd.ExcelWriter(f'{OUTPUT_FOLDER}comparison_file_IZ.xlsx', engine='xlsxwriter')
     DIFF.to_excel(writer, index=False, sheet_name='DIFF')
     SAME.to_excel(writer, index=False, sheet_name='SAME')
     workbook = writer.book
@@ -91,10 +108,9 @@ def compare_OCLC(): #Copied from UWO code
     writer.close()
 
 def main():
+    merge_reports_old()
     merge_reports()
-    compare_OCLC()
-
-print(__name__)
+    # compare_OCLC()
 
 if __name__ == '__main__':
     main()
